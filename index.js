fa9d6ce0b174d60c2ca3eacfd94a1d711b81c99f
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-05-07 06:09:51 +0600

    Added midlewares and mongoDB.

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
+// mongoDB config
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oiekl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 // Root API link GET request
 app.get("/", (req, res) => {
   res.send("Hello Inventoryish!");

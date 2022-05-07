0d294f996f529178d20531d51044c4768eb8bc8f
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-05-07 11:59:38 +0600

    Connected MongoDB and get inventory collections successfully.

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 4000;
+const app = express();
+
+// Initiate Middleware
+app.use(cors());
+app.use(express.json());
 
 // mongoDB config
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oiekl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const inventoryCollections = client
+      .db("inventoryish")
+      .collection("inventory");
+
+    app.get("/inventory", async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollections.find(query);
+
+      const inventory = await cursor.toArray();
+
+      res.send(inventory);
+    });
+  } finally {
+  }
+};
+
+run().catch(console.dir);
 
 // Root API link GET request
 app.get("/", (req, res) => {

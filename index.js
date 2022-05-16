4b556f78e6c96535ee16a64b8018bd3c5dbb6c5b
Md Amir Hossain <amirhossain.limon@gmail.com>
2022-05-16 22:56:04 +0600

    Latest code in Desktop

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const app = express();
 
 // Initiate Middleware
